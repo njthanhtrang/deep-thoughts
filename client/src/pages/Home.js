abe217bfac73f3680cf8d91a3b6c243c56cd0d29
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-06-21 00:25:33 -0700

    comments

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ import { QUERY_THOUGHTS } from "../utils/queries";
 import ThoughtList from "../components/ThoughtList";
 
 const Home = () => {
-  // use useQuery hook to make query request
+  // use useQuery hook to make query request from apollo server
   // when load Home component, execute query for thought data
   // async loading property. when finished, info stored in data property
   const { loading, data } = useQuery(QUERY_THOUGHTS);
