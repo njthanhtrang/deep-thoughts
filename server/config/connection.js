2cb019bce68aeb4b6601c54eb852fa9b1565b767
njthanhtrang <njthanhtrang@users.noreply.github.com>
2021-06-22 17:52:09 -0700

    add build script for Heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,9 @@
 const mongoose = require('mongoose');
 
+// in production, express.js uses process.env.PORT instead of 3001
+// Heroku won't let us use CRA default port of 3000
+// don't want to run nodemon or CRA bc neither codebase dynamically changes once deployed
+// we only need static Node.js server that serves built/compiled front-end files
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
